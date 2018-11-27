5f694984dffede7f6d803b4261db0d21dd7a90bf
JD Wolk <jd@philosophie.is>
2018-11-26 21:06:26 -0800

    Adds more GameState lenses

--- src/Annihilator/Core/GameLoop.purs
+++ src/Annihilator/Core/GameLoop.purs
@@ -11,28 +11,61 @@ import Data.Either (Either(..))
 import Data.Lens (lens, view, set)
 import Annihilator.Core.Cards
 
-data GamePhase = BeamPhase | ColliderPhase | RefreshPhase | GameOver
-data GameState =
-  GameState { currentPhase :: GamePhase }
-
 type BeamDeck = Array Card
+type DiscardPile = Array Card
+type AnnihilatedPile = Array Card
+type BackgroundCards = Array Card
+type DetectorCards = Array Card
+type ColliderCards = Array Card
+
+data GamePhase = NewGame | BeamPhase | ColliderPhase | RefreshPhase | GameOver
+data GameState =
+  GameState { currentPhase :: GamePhase
+            , beamDeck :: BeamDeck
+            , discard :: DiscardPile
+            , annihilated :: AnnihilatedPile
+            , background :: BackgroundCards
+            , detector :: DetectorCards
+            , collider :: ColliderCards
+            }
 
 derive instance eqGamePhase :: Eq GamePhase
 derive instance ordGamePhase :: Ord GamePhase
 derive instance eqGameState :: Eq GameState
 
 instance showGamePhase :: Show GamePhase where
+  show NewGame = "NewGame"
   show BeamPhase = "BeamPhase"
   show ColliderPhase = "ColliderPhase"
   show RefreshPhase = "RefreshPhase"
   show GameOver = "GameOver"
 
 instance showGameState :: Show GameState where
-  show (GameState { currentPhase }) = "GameState { currentPhase: " <> show currentPhase <> " }"
+  show (GameState { currentPhase, beamDeck, discard, annihilated, background, detector, collider }) =
+    "GameState { currentPhase: " <> show currentPhase <> ", " <>
+               " beamDeck: " <> show beamDeck <> ", " <>
+               " discard: " <> show discard <> ", " <>
+               " annihilated: " <> show annihilated <> ", " <>
+               " background: " <> show background <> ", " <>
+               " detector: " <> show detector <> ", " <>
+               " collider: " <> show collider <> ", " <>
+               " }"
+
+newGameState :: BeamDeck -> GameState
+newGameState beamDeck =
+  GameState { currentPhase: NewGame
+            , beamDeck
+            , discard: []
+            , annihilated: []
+            , background: []
+            , detector: []
+            , collider: []
+            }
 
 validPhaseTransitions :: Map.Map GamePhase (Array GamePhase)
 validPhaseTransitions =
-  Map.fromFoldable [ Tuple BeamPhase [ColliderPhase, RefreshPhase, GameOver]
+  Map.fromFoldable [ Tuple NewGame [BeamPhase]
+                   , Tuple BeamPhase [ColliderPhase, RefreshPhase, GameOver]
                    , Tuple ColliderPhase [GameOver, BeamPhase]
                    , Tuple GameOver [] :: Tuple GamePhase (Array GamePhase)
                    ]
@@ -44,14 +77,33 @@ transitionPhase current next = result where
            else Left $ "Cannot transition game phase from '" <> show current <> "' to '" <> show next
   nextPhases = unsafePartial $ fromJust $ Map.lookup current validPhaseTransitions
 
-transition :: GamePhase -> GameState -> Either String GameState
-transition nextPhase gs = result where
+gsTransitionPhase :: GamePhase -> GameState -> Either String GameState
+gsTransitionPhase nextPhase gs = result where
   result = (flip gsSetCurrentPhase) gs <$> validatedNextPhase
   validatedNextPhase = transitionPhase (gsCurrentPhase gs) nextPhase
 
 -- lenses
 _gameState = lens (\(GameState gs) -> gs) (\_ -> GameState)
 _currentPhase = lens _.currentPhase $ _ { currentPhase = _ }
+_discard = lens _.discard $ _ { discard = _ }
+_beamDeck = lens _.beamDeck $ _ { beamDeck = _ }
+_annihilated = lens _.annihilated $ _ { annihilated = _ }
+_background = lens _.background $ _ { background = _ }
+_detector = lens _.detector $ _ { detector = _ }
+_collider = lens _.collider $ _ { collider = _ }
 
 gsCurrentPhase = view (_gameState <<< _currentPhase)
+gsDiscard = view (_gameState <<< _discard)
+gsBeamDeck = view (_gameState <<< _beamDeck)
+gsAnnihilated = view (_gameState <<< _annihilated)
+gsBackground = view (_gameState <<< _background)
+gsDetector = view (_gameState <<< _detector)
+gsCollider = view (_gameState <<< _collider)
+
 gsSetCurrentPhase = set (_gameState <<< _currentPhase)
+gsSetDiscard = set (_gameState <<< _discard)
+gsSetBeamDeck = set (_gameState <<< _beamDeck)
+gsSetAnnihilated = set (_gameState <<< _annihilated)
+gsSetBackground = set (_gameState <<< _background)
+gsSetDetector = set (_gameState <<< _detector)
+gsSetCollider = set (_gameState <<< _collider)
