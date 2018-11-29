c9b48c225d3eb8b8f9beb71f03ad966c965ec06b
JD Wolk <jd@philosophie.is>
2018-11-28 20:40:03 -0800

    Reimplements allSameColor

--- src/Annihilator/Core/Confinements.purs
+++ src/Annihilator/Core/Confinements.purs
@@ -1,9 +1,9 @@
 module Annihilator.Core.Confinements where
 
 import Prelude
-import Data.Foldable (all)
-import Data.Array (head, filter, length)
-import Annihilator.Core.Cards (Card, qcColor, qcColors, qcIsRegular, qcIsAnti, allColors)
+import Data.Foldable (all, any)
+import Data.Array (filter, length)
+import Annihilator.Core.Cards (Card, Color, qcColor, qcColors, qcIsRegular, qcIsAnti, allColors)
 
 allRegular :: Array Card -> Boolean
 allRegular = all qcIsRegular
@@ -11,22 +11,21 @@ allRegular = all qcIsRegular
 allAnti :: Array Card -> Boolean
 allAnti = all qcIsAnti
 
--- TODO: check if the count of one color is the same as the length of qcs
+colorsMatching :: Color -> Array Card -> Array Color
+colorsMatching color cards = filter ((==) color) $ qcColors cards
+
 allSameColor :: Array Card -> Boolean
-allSameColor qcs = result where
-  maybeFirstColor = join $ qcColor <$> (head qcs)
-  allColor maybeColor xs = all (\qc -> qcColor qc == maybeColor) xs
-  result = allColor maybeFirstColor qcs
+allSameColor cards = any allCardsHaveColor allColors where
+  allCardsHaveColor color = (length cards) == (length $ colorsMatching color cards)
 
 oneOfEachColor :: Array Card -> Boolean
 oneOfEachColor cards = all onlyOnce allColors where
-  colorsMatching c = filter ((==) c)
-  onlyOnce color = (==) 1 $ length $ colorsMatching color $ qcColors cards
+  onlyOnce color = (==) 1 $ length $ colorsMatching color cards
 
 lengthIs :: forall a . Int -> Array a -> Boolean
 lengthIs n xs = (length xs) == n
 
 isValidConfinement :: Array Card -> Boolean
 isValidConfinement cards = (lengthIs 3 cards && allRegular cards && oneOfEachColor cards)
-                      || (lengthIs 3 cards && allAnti cards && oneOfEachColor cards)
-                      || (lengthIs 2 cards && allSameColor cards)
+                       || (lengthIs 3 cards && allAnti cards && oneOfEachColor cards)
+                       || (lengthIs 2 cards && allSameColor cards)
