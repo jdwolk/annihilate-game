44a3eff35e4a5d114fe7ae2cf4c8862ecd365b14
JD Wolk <jd@philosophie.is>
2018-11-29 22:01:21 -0800

    Adds hedwig for UI + adds hot reloading

--- src/Main.purs
+++ src/Main.purs
@@ -2,25 +2,7 @@ module Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
-
-import Annihilator.Core.Cards
-import Annihilator.Core.Confinements
+import Annihilator.UI.Main as AnnihilatorMain
 
 main :: Effect Unit
-main = do
-  redUp       <- pure $ quarkCard Regular Red Up
-  blueStrange <- pure $ quarkCard Regular Blue Strange
-  greenDown   <- pure $ quarkCard Regular Green Down
-  antiRedDown <- pure $ quarkCard Anti Red Down
-  annihilate  <- pure AnnihilateCard
-
-  shouldBeValid <- pure $ isValidConfinement [redUp, antiRedDown]
-  alsoValid     <- pure $ isValidConfinement [blueStrange, redUp, greenDown]
-  invalid       <- pure $ isValidConfinement [blueStrange, greenDown, antiRedDown]
-  alsoInvalid   <- pure $ isValidConfinement [annihilate, redUp, antiRedDown]
-
-  log $ "Valid: " <> (show shouldBeValid)
-  log $ "Valid: " <> (show alsoValid)
-  log $ "Invalid: " <> (show $ not invalid)
-  log $ "Invalid: " <> (show $ not alsoInvalid)
+main = AnnihilatorMain.main
---
+++ src/OldMain.purs
@@ -0,0 +1,26 @@
+module OldMain where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+import Annihilator.Core.Cards
+import Annihilator.Core.Confinements
+
+oldMain :: Effect Unit
+oldMain = do
+  redUp       <- pure $ quarkCard Regular Red Up
+  blueStrange <- pure $ quarkCard Regular Blue Strange
+  greenDown   <- pure $ quarkCard Regular Green Down
+  antiRedDown <- pure $ quarkCard Anti Red Down
+  annihilate  <- pure AnnihilateCard
+
+  shouldBeValid <- pure $ isValidConfinement [redUp, antiRedDown]
+  alsoValid     <- pure $ isValidConfinement [blueStrange, redUp, greenDown]
+  invalid       <- pure $ isValidConfinement [blueStrange, greenDown, antiRedDown]
+  alsoInvalid   <- pure $ isValidConfinement [annihilate, redUp, antiRedDown]
+
+  log $ "Valid: " <> (show shouldBeValid)
+  log $ "Valid: " <> (show alsoValid)
+  log $ "Invalid: " <> (show $ not invalid)
+  log $ "Invalid: " <> (show $ not alsoInvalid)
